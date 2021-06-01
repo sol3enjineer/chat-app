36e4b0ddf100e853cabd0a2264a2ac9b7f3aab65
sol3enjineer <f96moribe@gmail.com>
2021-06-01 17:12:58 +0900

    メンター変更

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,11 @@
 class Message < ApplicationRecord
     belongs_to :room
     belongs_to :user
-    has_many_attached :image
-
-    validates :content, presence: true
-end
+    has_one_attached :image
+  
+    validates :content, presence: true, unless: :was_attached?
+  
+    def was_attached?
+      self.image.attached?
+    end
+  end
